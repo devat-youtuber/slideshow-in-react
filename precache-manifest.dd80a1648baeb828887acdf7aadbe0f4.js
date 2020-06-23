64ef05cd3ca2b755dcc89b86ccbe4057f58d5880
dev at <battuosai01@gmail.com>
2020-06-22 17:38:20 -0700

    Updates

---
+++ precache-manifest.dd80a1648baeb828887acdf7aadbe0f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0076d52ee2c91c5b3e855d86a8c956",
+    "url": "/slideshow-in-react/index.html"
+  },
+  {
+    "revision": "00d9f3c9b68f155a5a13",
+    "url": "/slideshow-in-react/static/css/main.ba72d20b.chunk.css"
+  },
+  {
+    "revision": "ba151ec525cb078d42b6",
+    "url": "/slideshow-in-react/static/js/2.807ffe4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slideshow-in-react/static/js/2.807ffe4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00d9f3c9b68f155a5a13",
+    "url": "/slideshow-in-react/static/js/main.65fd6eae.chunk.js"
+  },
+  {
+    "revision": "eaf2df8fbde1e649d997",
+    "url": "/slideshow-in-react/static/js/runtime-main.d995d3af.js"
+  }
+]);
